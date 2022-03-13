d2b52f2c07fc48ace8331fda2e55e9c678ff86df
Victoria <Bernard.victoria22@gmail.com>
2022-03-13 15:15:47 -0500

    quadPod

--- src/tower.ts
+++ src/tower.ts
@@ -16,7 +16,7 @@ function move(state: GameState, target: number, destination: number): GameState 
     }
     //is legal
     let destPole = state.poles[destination]
-    let destDisk = destPole[destPole.length - 1]
+    let destDisk = destPole.length ? destPole[destPole.length - 1] : -1
     if (targetDisc < destDisk) {
         throw new Error(`That was an illegal move there  ${target} <  ${destDisk}`)
     }
@@ -67,7 +67,7 @@ function changeState(state: GameState) {
 }
 
 const moveTriPod = (state: GameState, target: number, destination: number) => {
-
+    let moves: [number, number][] = [[target, destination]]
     //whatever is not target or destination
     let fillerPole = [0, 1, 2].reduce((pole, cur) => (pole !== target && pole !== destination) ? pole : cur)
     console.log('fillerPole', fillerPole);
@@ -97,30 +97,87 @@ async function main() {
     let iSleeps = 0
     function isSolved(state: GameState) {
         //TODO: fill in logic
-        return iSleeps < 1
+        return iSleeps > 100
     }
     document.addEventListener('DOMContentLoaded', async (_e) => {
         changeState(gameState)
+        let i = 0
         // moveTriPod(initGameState, 0, 1)
         // setTimeout(() => changeState(moveTriPod(initGameState, 0, 1)), 1000)
-        while (isSolved(gameState)) {
-            console.log('iSleeps', iSleeps);
-
-            performNextMove(gameState)
-            await sleep(500)
-            iSleeps = iSleeps++
+        while (!isSolved(gameState)) {
+            iSleeps = ++iSleeps
+            performNextMove(gameState, i)
+            i++
+            await sleep(100)
 
             //onceSolved
         }
+        console.log('i', i);
+
     })
 
 
 }
 main()
+/**
+ * 
+ * @param state 
+ * @returns the next big disk that is eligle to move
+ */
+function findNextBiggie(state: GameState): number {
+    return 0
+}
+/**
+ * 
+ * @param state 
+ * @returns the next big disk that is eligle to move
+ */
+function spreadChildren(state: GameState): number {
+    return 0
+}
+type moves = [number, number][]
 
-function performNextMove(state: GameState) {
+function getMovesForTripod(target: number, destination: number): moves {
+    const fillerPole = [0, 1, 2].reduce((pole, cur) => (pole !== target && pole !== destination) ? pole : cur)
+    return [[target, destination], [target, fillerPole],
+    [destination, fillerPole],
+    [target, destination], [fillerPole, target], [fillerPole, destination],
+    [target, destination]]
+}
+function getMovesForQuadpod(target: number, destination: number): moves {
+    const fillerPole = [0, 1, 2].reduce((pole, cur) => (pole !== target && pole !== destination) ? pole : cur)
+    return [...getMovesForTripod(target, fillerPole), [target, destination], ...getMovesForTripod(fillerPole, destination)]
+}
+function performNextMove(state: GameState, moveI: number) {
     //call move based on state
-    let newState = moveTriPod(state, 0, 1)
+    // let newState = moveTriPod(state, 0, 1)
+    console.log('moveI', moveI);
+
+
+    if (true/**if first move  */) {
+        let firstMoves: moves = [
+            //move Tripod
+            ...getMovesForTripod(0, 1),   //[0, 1], [0, 2], [1, 2], [0, 1], [2, 0], [2, 1], [0, 1],
+
+            //move 3 biggie
+            [0, 2],
+            ...getMovesForTripod(1, 2),
+
+            //move it's brother 2
+            [0, 1],
+            ...getMovesForTripod(2, 0),
+            [2, 1],
+            ...getMovesForTripod(0, 1),
+
+            //move brother 1
+            ...getMovesForQuadpod(1, 2)
+
+        ]
+        console.log('firstMoves.length', firstMoves.length);
+
+        move(state, firstMoves[moveI][0], firstMoves[moveI][1])
+    }
+
     // let newState = move(state, 0, 1)
     renderToDOM(renderGame(state))
 }
