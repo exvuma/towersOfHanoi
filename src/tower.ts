f9a14759d0a3c07660c5d06f043c9bbf3bac148e
Victoria <Bernard.victoria22@gmail.com>
2022-03-13 12:51:54 -0500

    clean up and move works

--- src/tower.ts
+++ src/tower.ts
@@ -3,7 +3,7 @@ let s2: Array<number> = []
 let s3: Array<number> = []
 // let s2 = [0, 0, 0, 0]
 // let s3 = [0, 0, 0, 0]
-const gameState1: GameState = {
+const initGameState: GameState = {
     size: 7,
     poles: [
         [0, 3],
@@ -20,36 +20,21 @@ const gameState: GameState = {
     ]
 }
 document.addEventListener('DOMContentLoaded', _e => {
-    changeState(gameState1)
+    console.log("DOMContentLoaded")
+    changeState(initGameState)
 })
 
 
 function move(state: GameState, target: number, destination: number): GameState {
-    let poles: GameState['poles'] = [[11], [22], [33]]// fake array
-    let polesM: GameState['poles'] = poles
-    state.poles.forEach((p, i) => {
-        // const poles: GameState['poles'] = [...state.poles.map((p) => {
-        polesM[i] = p.reverse()
-    })
-    // const poles: GameState['poles'] = state.poles.reduce((p, _cur, _curIndex, _arr) => {
-    //     // const poles: GameState['poles'] = [...state.poles.map((p) => {
-    //     console.log("sds", p)
-    //     return [1]////p.reverse()
-    //     // return [...p].reverse()
-    // })
-
-    const targetDisc = poles[target].pop()
+    const targetDisc = state.poles[target].pop()
 
     if (!targetDisc) {
         throw new Error(`Cannot use this pole target ${target}`)
     }
 
-    poles[destination].push(targetDisc)
+    state.poles[destination].push(targetDisc)
 
-    return {
-        ...state,
-        poles,
-    }
+    return state
 }
 
 type GameState = {
@@ -62,88 +47,34 @@ type GameState = {
 }
 // <div class="disc disc-n-${pole.map((disc) => pole[disc])}"></div>
 function renderGame(state: GameState): string {
-    // state = moveSmally(gameState1)
+    // state = moveSmally(initGameState)
     return `<div class="poles poles-n-${state.size}">
         ${state.poles.map(pole => renderPole(pole)).join('\n')}
     </div>`
 }
 
 function renderPole(pole: number[]): string {
-    console.log(pole)
+    // console.log('pole rendered', pole)
     return `<div class="pole">
-        ${pole.reverse().map(disc => renderDisc(disc)).join('\n')}
+        ${pole.slice(0).reverse().map(disc => renderDisc(disc)).join('\n')}
     </div>`
 }
 
 function renderDisc(disc: number): string {
-    return `<div class="disc disc-n-${disc}"></div>`
+    return `<div class="disc disc-n-${disc}">${disc}</div>`
 }
 
 function renderToDOM(html: string) {
     document.body.innerHTML = html
 }
 
-const moveChildren = (_n: number) => {
 
-    //not logic, just type safety bullshit 
-    const child = s1[3]
-    const child2 = s2[3]
-    if (typeof (child) !== "number") {
-        return ''
-    }
-    if (typeof (child2) !== "number") {
-        return ''
-    }
 
-    if (s1[0] > s2[0]) {
-
-        s1.pop()
-        s2.push(child)
-
-    }
-    if (s1[0] > s3[0]) {
-        if (s3[3] === 0) {
-            s3.pop()
-        }
-
-        // s3.push(s1.pop())
-
-    }
-    return "balh"
-
-}
-// console.log(moveChildren(3));
-function moveTriPod(_gameState: GameState, fromPole: number, _newPole: number): GameState {
-    //TODO check where tripod exists
-    // assuming tripod at pole 1...
-    let i = 0;
-    for (i = 0; i < 0; i++) {
-
-    }
-    const newFromPole = gameState1.poles[fromPole]//.pop().pop().pop()
-    // newPole = 
-    let newPoles: [number[], number[], number[]] = [[], [], []]
-
-    newPoles[fromPole] = newFromPole
-    return {
-        size: 7,
-        poles: newPoles
-    }
-}
-function moveSmally(gameState: GameState, fromPole: number = 0, newPole: number = 2): GameState {
-    let newPoles: [number[], number[], number[]] = [[], [], []]
-    let newGameState: GameState = { ...gameState, poles: [...gameState.poles] }
-    newGameState.poles[fromPole] = newGameState.poles[fromPole].slice(0, 2)
-    newGameState.poles[newPole] = newGameState.poles[newPole].slice(0, 2)
-    console.log(
-        'smalls has landed', newGameState
-    )
-    return newGameState
-}
 function changeState(newGameState: GameState) {
+    console.log('newGameState', newGameState.poles);
+
+
     renderToDOM(renderGame(newGameState))
 }
 
 
-console.log(s3)
-// changeState(move(gameState1, 1, 2))
