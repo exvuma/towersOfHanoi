4b82b8008c66fecb5145ef216335bdc327617bf5
Victoria <Bernard.victoria22@gmail.com>
2022-03-13 13:19:12 -0500

    moveTripod working and poles styles

--- src/tower.ts
+++ src/tower.ts
@@ -6,9 +6,7 @@ let s3: Array<number> = []
 const initGameState: GameState = {
     size: 7,
     poles: [
-        [0, 3],
-        [1, 2, 4],
-        [5, 6],
+        [0, 1, 2, 3, 4, 5, 6], [], []
     ]
 }
 const gameState: GameState = {
@@ -20,15 +18,15 @@ const gameState: GameState = {
     ]
 }
 document.addEventListener('DOMContentLoaded', _e => {
-    console.log("DOMContentLoaded")
     changeState(initGameState)
+    setTimeout(() => changeState(moveTriPod(initGameState)), 1000)
 })
 
 
 function move(state: GameState, target: number, destination: number): GameState {
     const targetDisc = state.poles[target].pop()
 
-    if (!targetDisc) {
+    if (!targetDisc && targetDisc !== 0) {
         throw new Error(`Cannot use this pole target ${target}`)
     }
 
@@ -49,13 +47,13 @@ type GameState = {
 function renderGame(state: GameState): string {
     // state = moveSmally(initGameState)
     return `<div class="poles poles-n-${state.size}">
-        ${state.poles.map(pole => renderPole(pole)).join('\n')}
+        ${state.poles.map((pole, i) => renderPole(pole, i)).join('\n')}
     </div>`
 }
 
-function renderPole(pole: number[]): string {
-    // console.log('pole rendered', pole)
+function renderPole(pole: number[], i: number): string {
     return `<div class="pole">
+        <div class="number">${i}</div>
         ${pole.slice(0).reverse().map(disc => renderDisc(disc)).join('\n')}
     </div>`
 }
@@ -71,10 +69,17 @@ function renderToDOM(html: string) {
 
 
 function changeState(newGameState: GameState) {
-    console.log('newGameState', newGameState.poles);
-
-
     renderToDOM(renderGame(newGameState))
 }
 
 
+const moveTriPod = (state: GameState) => {
+    let i = 0;
+    for (i = 0; i < 3; i++) {
+
+    }
+    const spreadMove = move(move(move(move(move(state, 0, 1), 0, 2), 1, 2), 0, 1), 0, 0)
+    // const secMoves = spreadMove//move(move(move(spreadMove, 0, 1), 0, 2), 1, 2)
+    const secMoves = move(move(move(spreadMove, 2, 0), 2, 1), 0, 1)
+    return move(secMoves, 0, 0)
+}
